72ced253dee902f6ab2b27dd9a1c3fe101285fa9
hoapv <hoa.np.tk@gmail.com>
2021-06-13 15:45:30 +0700

    crash at insmod

--- blink-led/blink-led.c
+++ blink-led/blink-led.c
@@ -18,13 +18,28 @@
 #include <linux/cdev.h>         /**/
 #include <linux/slab.h>         /* kmaloc, kfree */
 #include <linux/uaccess.h>      /**/
-
+#include <asm/io.h>
+#include <linux/gpio.h>
+#include <linux/delay.h>
+#include <linux/ioport.h>
 
 #define DRIVER_AUTHOR   "HoaPV - hoa.pv127@gmail.com"
 #define DRIVER_DESC     "this driver controls blinking a led"
 #define DRIVER_VERSION  "0.1"
 
 #define STATUS_LEN  16
+#define GPIO_BASE   0x3F200000
+#define BLOCKSIZE   4096
+// #define LED         (29)
+
+#define CLEAR_REG(reg)  *(volatile unsigned int *)(reg) &= (unsigned int)0x00000000
+#define LED_INPUT(b)    *(volatile unsigned int *)(0x00000000+b) &= (unsigned int) 0xFFFF8FFF
+#define LED_OUTPUT(b)   *(volatile unsigned int *)(0x00000000+b) |= (unsigned int) 0x00001000
+
+#define SET_LED(b)      *(volatile unsigned int *)(0x0000001C+b) |= 0x10
+#define CLEAR_LED(b)    *(volatile unsigned int *)(0x00000028+b) |= 0x10
+
+volatile unsigned int * gpio = NULL;
 
 struct _blink_led_dev_t {
     unsigned char * status; 
@@ -122,9 +137,15 @@ static ssize_t blink_led_write(struct file *filp, const char __user *buff, size_
 
     if(!strncmp((const char *)kernel_buff, "start", strlen("start"))) {
         strcpy(blink_led_drv.blink_led_data->status, "start");
+
+        LED_OUTPUT(gpio);
+        SET_LED(gpio);
     }
     else if(!strncmp((const char *)kernel_buff, "stop", strlen("stop"))) {
         strcpy(blink_led_drv.blink_led_data->status, "stop");
+        
+        LED_OUTPUT(gpio);
+        CLEAR_LED(gpio);
     }
     else {
         pr_err("%s: Wrong command\n", kernel_buff);
@@ -189,10 +210,38 @@ static int __init blink_led_drv_init(void) {
         pr_err("Cannot add a character device to the system\n");
         goto failed_alloc_cdev;
     }
+    
+    pr_info("PHYSICAL ADDRESS = 0x%X\n", (GPIO_BASE + 8));
+    msleep(100);
+    gpio = ioremap(GPIO_BASE, BLOCKSIZE);
+    if(gpio == NULL) {
+        pr_err("Cannot map GPIO\n");
+        goto failed_gpio;   
+    }
+    pr_info("VIRTUAL ADDRESS = 0x%X\n", gpio + 8);
+    msleep(100);
+
+    // CLEAR_REG(gpio + 0x8);
+    // CLEAR_REG(gpio + 0x1C);
+    // CLEAR_REG(gpio + 0x28);
+    LED_INPUT(gpio);
+    msleep(100);
+    pr_info("SET gpio as input\n");
+    LED_OUTPUT(gpio);
+    msleep(100);
+    pr_info("SET gpio as output\n");
+    SET_LED(gpio);
+    msleep(100);
+    pr_info("SET gpio output at high level\n");
+
+    pr_info("value of 0x0 = 0x%X\n", *(gpio));
 
     strncpy(blink_led_drv.blink_led_data->status, "stop", strlen("stop"));
     pr_info("Initialize blink led driver successfully\n");
     return 0;
+
+failed_gpio:
+    cdev_del(blink_led_drv.led_cdev);
 
 failed_alloc_cdev:
     blink_led_dev_free(blink_led_drv.blink_led_data);
@@ -216,6 +265,8 @@ failed_alloc_device_number:
 /*  remove driver */
 static void __exit blink_led_drv_exit(void) {
 
+    // gpio_free(LED);
+    iounmap(gpio);
     cdev_del(blink_led_drv.led_cdev);
 
     blink_led_dev_free(blink_led_drv.blink_led_data);
